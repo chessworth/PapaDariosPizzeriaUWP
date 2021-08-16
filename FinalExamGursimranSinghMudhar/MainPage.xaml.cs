f7c488a8d207f2941688ac8d3eaf511faf841440
chessworth <mugtheart@gmail.com>
2021-08-16 00:49:30 -0400

    Inital Commit - Background and basic layout, some user logic.

---
+++ FinalExamGursimranSinghMudhar/MainPage.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Data.SqlClient;
+using System.Data;
+using Windows.UI.Popups;
+using System.Text;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FinalExamGursimranSinghMudhar
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        //set up connection string as property
+        private static string connectionString = @"Server=DESKTOP-DAK1RS1;Database=final;User Id=Gursimran;Password=123456;";
+        public string ConnectionString { get => connectionString; set =>connectionString = value; }
+        public bool LoggedIn { get; set; }
+        public bool admin { get; set; }
+        public User user { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+        }
+        //methods for password decode/encode (Stack Overflow + .NET DOCS - System.Text)
+        public static string Base64Encode(string plainText)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(plainText));
+        }
+        public static string Base64Decode(string base64EncodedData)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(base64EncodedData));
+        }
+        private async void login_submit(object sender, RoutedEventArgs e)
+        {
+            string login = username.Text;
+            string pw = Base64Encode(password.Password);
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            SqlCommand cmd = conn.CreateCommand();
+            try
+            {
+                string query = "select * from users where username = @0 and password = @1;";
+                cmd.CommandText = query;
+                cmd.Parameters.AddWithValue("@0", login);
+                cmd.Parameters.AddWithValue("@1", pw);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    User curLogged = new User();
+                    IDataReader data = (IDataReader)reader;
+                    curLogged.UserID = (int)data[0];
+                    curLogged.Fname = data[1].ToString();
+                    curLogged.Lname = data[2].ToString();
+                    curLogged.Username = data[3].ToString();
+                    admin = (bool)data[5];
+                    LoggedIn = true;
+                    user = curLogged;
+                    //TODO : Redirect to home
+                }
+                else
+                {
+                    MessageDialog dialog = new MessageDialog("Invalid Username or Password");
+                    await dialog.ShowAsync();
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                MessageDialog dialog = new MessageDialog(ex.Message.ToString());
+                await dialog.ShowAsync();
+            }
+            finally
+            {
+                cmd.Dispose();
+                conn.Close();
+            }
+        }
+
+        private void registerView_Click(object sender, RoutedEventArgs e)
+        {
+            loginGrid.Visibility = Visibility.Collapsed;
+            registerGrid.Visibility = Visibility.Visible;
+        }
+        private void loginView_Click(object sender, RoutedEventArgs e)
+        {
+            registerGrid.Visibility = Visibility.Collapsed;
+            loginGrid.Visibility = Visibility.Visible;
+        }
+        private async void register_submit(object sender, RoutedEventArgs e)
+        {
+            string fname = firstname.Text;
+            string lname = lastname.Text;
+            string login = newusername.Text;
+            string pw = Base64Encode(newpassword.Password);
+            string confirmpw = Base64Encode(newpasswordconfirm.Password);
+            if(fname == "" || lname == "" || login == "" || pw == "" || confirmpw == "")
+            {
+                //TODO add red border to empty inputs
+                MessageDialog dialog = new MessageDialog("Please enter all values");
+                await dialog.ShowAsync();
+                return;
+            }
+            if (!pw.Equals(confirmpw))
+            {
+                //TODO add red border to empty inputs
+                MessageDialog dialog = new MessageDialog("Passwords don't match!");
+                await dialog.ShowAsync();
+                return;
+            }
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            SqlCommand cmd = conn.CreateCommand();
+            try
+            {
+                string query = "INSERT INTO USERS(FNAME,LNAME,USERNAME,PASSWORD) VALUES(@0, @1, @2, @3); ";
+                cmd.CommandText = query;
+                cmd.Parameters.AddWithValue("@0", fname);
+                cmd.Parameters.AddWithValue("@1", lname);
+                cmd.Parameters.AddWithValue("@2", login);
+                cmd.Parameters.AddWithValue("@3", pw);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    User curLogged = new User();
+                    IDataReader data = (IDataReader)reader;
+                    curLogged.UserID = (int)data[0];
+                    curLogged.Fname = data[1].ToString();
+                    curLogged.Lname = data[2].ToString();
+                    curLogged.Username = data[3].ToString();
+                    admin = (bool)data[5];
+                    LoggedIn = true;
+                    user = curLogged;
+                    //TODO : Redirect to home
+                }
+                else
+                {
+                    MessageDialog dialog = new MessageDialog("Invalid Username or Password");
+                    await dialog.ShowAsync();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageDialog dialog = new MessageDialog(ex.Message.ToString());
+                await dialog.ShowAsync();
+            }
+            finally
+            {
+                cmd.Dispose();
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ FinalExamGursimranSinghMudhar/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExamGursimranSinghMudhar
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string Username { get; set; }
+    }
+}
