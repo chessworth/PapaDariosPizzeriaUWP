e94c429b8553c94b11c94c1202f40dd691aa362a
chessworth <mugtheart@gmail.com>
2021-08-19 00:31:22 -0400

    mid-work commit

--- FinalExamGursimranSinghMudhar/MainPage.xaml.cs
+++ FinalExamGursimranSinghMudhar/MainPage.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using System.Data.SqlClient;
 using System.Data;
 using Windows.UI.Popups;
@@ -200,6 +189,15 @@ namespace FinalExamGursimranSinghMudhar
             Hide_all();
             var page = (StackPanel)((Button)sender).Tag;
             page.Visibility = Visibility.Visible;
+        }
+        private async void Feedback_submit(object sender, RoutedEventArgs e)
+        {
+            if (!loggedin)
+            {
+                MessageDialog dialog = new MessageDialog("You must be logged in to sumit feedback");
+                await dialog.ShowAsync();
+                return;
+            }
         }
         //return visibility set
         private Visibility GetVisibility(bool visible)
